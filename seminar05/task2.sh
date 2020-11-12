fe209e6c65b0ecf878ae75f823e21e96636fd49e
MKlimovich <alexydiptrih@gmail.com>
2020-11-12 12:36:11 +0300

    Do it

--- seminar05/task2.sh
+++ seminar05/task2.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+read hash1
+read hash2
+
+git log --pretty=format:%s $hash2..$hash1 > "release_notes.md"
--- seminar05/task3.sh
+++ seminar05/task3.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+read commit
+
+echo "Commit is:"
+echo $commit
+
+if [[ "$commit" =~ "^issue-[0-9]{1,} *" ]];then
+  echo good commit!
+  exit 0
+else
+  echo wrong commit format
+  exit 1
+fi
